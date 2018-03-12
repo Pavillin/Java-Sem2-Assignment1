1c2f5d8fbfbf0bbcc6be99c4a514ccc47cb22e0c
Dylan <dylansprague11@hotmail.com>
2018-03-12 13:48:45 -0400

    Final

--- test/assignment1/CourseTest.java
+++ test/assignment1/CourseTest.java
@@ -225,4 +225,106 @@ public class CourseTest
         String result = course.toString();
         assertEquals(expResult, result);
     }
+
+    /**
+     * Test of setInstructor method, of class Course.
+     */
+    @Test
+    public void testSetInstructor()
+    {
+        Instructor instructor = new Instructor("Prof", "Test", "11 Sinsister St", "London", "L0L 1W4", LocalDate.of(2000, Month.SEPTEMBER, 12), LocalDate.of(1976, Month.MARCH, 12), 1); 
+        instructor.addCourseToAbilities("COMP1008");
+        course.setInstructor(instructor);
+        assertEquals(instructor, course.getInstructor());
+    }
+
+    
+    /**
+     * Test of addStudent method, of class Course.
+     */
+    @Test
+    public void testAddStudent()
+    {
+        Student student = new Student("Barb", "Dwyer1", "123 Happy St", "Barrie", "L4M 8T0", LocalDate.of(1976, Month.MARCH, 10), LocalDate.of(2016, Month.SEPTEMBER, 1), 10001);;
+        Student student2 = new Student("Barb", "Dwyer2", "123 Happy St", "Barrie", "L4M 8T0", LocalDate.of(1976, Month.MARCH, 10), LocalDate.of(2016, Month.SEPTEMBER, 1), 10001);;
+        Student student3 = new Student("Barb", "Dwyer3", "123 Happy St", "Barrie", "L4M 8T0", LocalDate.of(1976, Month.MARCH, 10), LocalDate.of(2016, Month.SEPTEMBER, 1), 10001);;
+
+        course.addStudent(student);
+        course.addStudent(student);
+        assertEquals(2, course.getStudents().size());
+    }
+    
+     /**
+     * Test of addStudent method, of class Course.
+     */
+    @Test
+    public void testAddStudentStudentInBadStanding()
+    {
+        Student student = new Student("Barb", "Dwyer1", "123 Happy St", "Barrie", "L4M 8T0", LocalDate.of(1976, Month.MARCH, 10), LocalDate.of(2016, Month.SEPTEMBER, 1), 10001);;
+        student.suspendStudent();
+        
+        try{
+            course.addStudent(student);    
+            fail("The student is in bad standing and should not have been added");
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.out.printf("Trying to add a student in bad standing: %s%n",e);
+        }
+    }
+
+     /**
+     * Test of addStudent method, of class Course.
+     */
+    @Test
+    public void testAddStudentInvalid()
+    {
+        Student student = new Student("Barb", "Dwyer2", "123 Happy St", "Barrie", "L4M 8T0", LocalDate.of(1976, Month.MARCH, 10), LocalDate.of(2016, Month.SEPTEMBER, 1), 10001);;
+        Student student2 = new Student("Barb", "Dwyer3", "123 Happy St", "Barrie", "L4M 8T0", LocalDate.of(1976, Month.MARCH, 10), LocalDate.of(2016, Month.SEPTEMBER, 1), 10001);;
+        Student student3 = new Student("Barb", "Dwyer3", "123 Happy St", "Barrie", "L4M 8T0", LocalDate.of(1976, Month.MARCH, 10), LocalDate.of(2016, Month.SEPTEMBER, 1), 10001);;
+
+        course.addStudent(student);
+        course.addStudent(student2);
+        try{
+            course.addStudent(student3);
+            fail("max # of students was 2, the third student should have triggered an exception");
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("Error for catching too many students: "+e);
+        }
+    }
+    
+    /**
+     * Test of setCourseName method, of class Course.
+     */
+    @Test
+    public void testSetCourseName()
+    {
+        String courseName = "COMP1111";
+        course.setCourseName(courseName);
+        assertEquals(courseName, course.getCourseName());
+    }
+
+    /**
+     * Test of setRoom method, of class Course.
+     */
+    @Test
+    public void testSetRoom()
+    {
+        String room = "A123";
+        course.setRoom(room);
+        assertEquals(room, course.getRoom());
+    }
+
+    /**
+     * Test of setMaxStudents method, of class Course.
+     */
+    @Test
+    public void testSetMaxStudents()
+    {
+        int maxStudents = 1;
+        course.setMaxStudents(maxStudents);
+        assertEquals(1, course.getMaxStudents());
+    }
 }
